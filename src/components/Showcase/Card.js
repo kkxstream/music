5dfcadbe3feca48361d5b56ef6c202aa018788c3
KK Xstream <156395658+kkxstream@users.noreply.github.com>
2024-07-22 02:46:41 +0530

    Update Card.js

--- src/components/Showcase/Card.js
+++ src/components/Showcase/Card.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card(props) { // name, img, key (id), onClick as props
     return (
-        <div className="m-auto p-4 lg:w-1/4 md:w-1/2" onClick={props.onClick}>
+        <div className="m-auto p-4 lg:w-3/4 md:w-2/4" onClick={props.onClick}>
             <div className="relative h-full flex flex-col items-center text-center">
                 <img className="hover:brightness-50 flex-shrink-0 rounded-lg w-screen h-56 object-cover object-center mb-4"
                     src={props.image} />
