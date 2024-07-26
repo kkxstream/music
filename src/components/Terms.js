9526236f952e0ab140f4c06c3c0f59530b767f5b
KK Xstream <156395658+kkxstream@users.noreply.github.com>
2024-07-27 01:52:33 +0530

    Update Terms.js

--- src/components/Terms.js
+++ src/components/Terms.js
@@ -12,7 +12,7 @@ function Terms(props) {
             <div className="container px-5 pt-28 pb-10 mx-auto text-gray-700 dark:text-gray-400 body-font bg-light-100 dark:bg-deep-900 ">
                 <div className="flex flex-col text-center w-full mb-10">
                     <h1 className='sm:text-3xl text-2xl font-medium title-font mb-4 text-black dark:text-white'>Terms of Use for DreamlyMusic</h1>
-                    <p className='text-lg sm:text-xl text-gray-900 dark:text-gray-100'>Welcome to DreamlyMusic, an online music streaming and downloading platform built for the sole purpose of personal use and learning. By accessing or using TuneStation, you agree to be bound by these Terms of Use. If you do not agree to these Terms of Use, please <b>do not</b> use TuneStation.</p>
+                    <p className='text-lg sm:text-xl text-gray-900 dark:text-gray-100'>Welcome to DreamlyMusic, an online music streaming and downloading platform built for the sole purpose of personal use and learning. By accessing or using DreamlyMusic, you agree to be bound by these Terms of Use. If you do not agree to these Terms of Use, please <b>do not</b> use DreamlyMusic.</p>
                 </div>
                 <hr />
                 <br />
@@ -27,9 +27,9 @@ function Terms(props) {
                     <br />
                     <li> <span className='font-bold text-gray-800 dark:text-gray-100'>&#8620; Ownership of data:</span> All the songs/albums/playlists displayed on this website belong to JioSaavn. We <b>do not intend</b> to infringe on any copyright, trademark, or other intellectual property rights of any third party. All rights to the music and other material available on DreamlyMusic belong to <b>JioSaavn</b> and the respective song labels.</li>
                     <br />
-                    <li> <span className='font-bold text-gray-800 dark:text-gray-100'>&#8620; No Piracy:</span> You agree not to use TuneStation to pirate music or engage in any other illegal activity.</li>
+                    <li> <span className='font-bold text-gray-800 dark:text-gray-100'>&#8620; No Piracy:</span> You agree not to use DreamlyMusic to pirate music or engage in any other illegal activity.</li>
                     <br />
-                    <li> <span className='font-bold text-gray-800 dark:text-gray-100'>&#8620; For JioSaavn and Copyright Owners:</span> In the event that JioSaavn or the copyright owners of the music available on DreamlyMusic want the website to be taken down for any copyright infringement related case, they may raise an issue at the <a href="https://github.com/codyandersan/DreamlyMusic" target="_blank" rel="noopener noreferrer" className='underline hover:text-black dark:hover:text-white'>TuneStation Git repository</a>. We will try our best to stop the functioning of this website as soon as possible.</li>
+                    <li> <span className='font-bold text-gray-800 dark:text-gray-100'>&#8620; For JioSaavn and Copyright Owners:</span> In the event that JioSaavn or the copyright owners of the music available on DreamlyMusic want the website to be taken down for any copyright infringement related case, they may raise an issue at the <a href="https://github.com/codyandersan/DreamlyMusic" target="_blank" rel="noopener noreferrer" className='underline hover:text-black dark:hover:text-white'>DreamlyMusic Git repository</a>. We will try our best to stop the functioning of this website as soon as possible.</li>
                 </ul>
 
                 <br/>
