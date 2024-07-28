3ece7e88e7f1766cedac59c8d16c081d979f7dde
KK Xstream <156395658+kkxstream@users.noreply.github.com>
2024-07-28 23:43:22 +0530

    Update Heading.js

--- src/components/Showcase/Heading.js
+++ src/components/Showcase/Heading.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Heading(props) {
     return (
         // The label for Showcase of songs and albums 
-        <div className="flex flex-col text-left w-full mb-2">
+        <div className="flex flex-col text-left w-full mb-4">
             <h1 className="text-2xl md:text-3xl font-medium title-font  dark:text-white text-black">{props.title}</h1>
             
         </div>
